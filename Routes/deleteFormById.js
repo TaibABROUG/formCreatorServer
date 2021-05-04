e89d92bb39f3919e738b26a49c622a1ff7085908
TaibABROUG <48461324+TaibABROUG@users.noreply.github.com>
2021-05-04 12:18:31 +0100

    fix delete form

--- Routes/deleteFormById.js
+++ Routes/deleteFormById.js
@@ -4,9 +4,12 @@ const db = require('../services/db');
 
 
 router.delete('/:idForm', (req, res, next) => {
-  const rows=  db.query('DELETE FROM forms where _id = ? ;', [req.params.idForm]).then((response)=> {
+  const rows=  db.query('DELETE FROM contents where idForm = ? ;', [req.params.idForm]).then((response)=> {
 
-    res.status(200).json({name: response.name});
+    const rows=  db.query('DELETE FROM forms where _id = ? ;', [req.params.idForm]).then((response)=> {
+
+      res.status(200).json({name: response.name});
+    }) ; 
   }) ; 
 
 });
